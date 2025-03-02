dbefb981a71388ebf9ee6c4417affecaa147f46b
Denzy <108025321+denzylegacy@users.noreply.github.com>
2025-03-01 23:20:13 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 
 TEMP_LIMIT=75
 
-TEMP_SAFE=60
-
 get_cpu_temp() {
   sensors | grep 'Package id 0:' | awk '{print $4}' | cut -d '+' -f2 | cut -d '.' -f1
 }
@@ -14,30 +12,23 @@ while true; do
   if [ -n "$TEMP" ] && [ "$TEMP" -gt "$TEMP_LIMIT" ]; then
     echo "----------------------------------------"
     echo " High Temperature Alert: ${TEMP}°C"
-    echo " Sleeping for 1m..."
+    echo " keyhunt will sleep for 3m..."
     echo "----------------------------------------"
-    sleep 1m
+    sleep 3m  # Rest for 3 minutes if temperature is high
   else
     if [ -n "$TEMP" ]; then
-      if [ "$TEMP" -le "$TEMP_SAFE" ]; then
-        echo "----------------------------------------"
-        echo " Temperature Safe: ${TEMP}°C"
-        echo " Running program..."
-        echo "----------------------------------------"
-      else
-        echo "----------------------------------------"
-        echo " Temperature Stable: ${TEMP}°C"
-        echo " Running program..."
-        echo "----------------------------------------"
-      fi
+      echo "----------------------------------------"
+      echo " Temperature Stable: ${TEMP}°C"
+      echo " Running program now..."
+      echo "----------------------------------------"
     else
       echo "----------------------------------------"
       echo " Warning: Unable to retrieve temperature"
       echo " Proceeding with program execution by default..."
       echo "----------------------------------------"
     fi
-    ./keyhunt -m bsgs -f tests/140.txt -b 140 -t 4 -s 10 -q -R &
-    sleep 1m
+    ./keyhunt -m bsgs -f tests/140.txt -b 140 -t 4 -s 10 -R &
+    sleep 10m
     pkill -f keyhunt
   fi
 done
